3abb5754c65ba847647e8580e0e89125f9552df8
Rashad Karanouh <11599358+rashad@users.noreply.github.com>
2021-09-14 16:01:23 +0200

    remove node modules'

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const state = {
+    clients : [
+        {id: 1, nom : "Machin1"},
+        {id: 2, nom : "Machin2"},
+        {id: 3, nom : "Machin3"}
+    ]
+  }
+  
+  const title = "bonjour"
+
+  const handleClick = () =>{
+    alert("salut la famille !")
+  }
+
   return (
     <div className="App">
-     
+      <div>
+                <h1>{title}</h1>
+                <button onClick={handleClick}>Click me</button>
+                <ul>
+                    {state.clients.map(client =>(
+                        <li>{client.nom} <button>X</button></li>
+                      ))}
+                </ul>
+                <form>
+                    <input type="text" placeholder="Ajouter un client"/><button>Confirmer</button>
+                </form>
+            </div>
     </div>
   );
 }
